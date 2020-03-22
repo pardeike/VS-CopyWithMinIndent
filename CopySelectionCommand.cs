b7a5819a99fbd43f22399acaf743080a76ed3e3c
Andreas Pardeike <andreas@pardeike.net>
2020-03-23 00:50:50 +0100

    first commit

---
+++ CopySelectionCommand.cs
@@ -0,0 +1,87 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Task = System.Threading.Tasks.Task;
+
+namespace CopyWithMinIndent
+{
+	internal sealed class CopySelectionCommand
+	{
+		public const int CommandId = 0x0100;
+		public static readonly Guid CommandSet = new Guid("2cc1eee4-d7b3-4c83-8679-a7952afb4737");
+		private readonly AsyncPackage package;
+
+		private CopySelectionCommand(AsyncPackage package, OleMenuCommandService commandService)
+		{
+			this.package = package ?? throw new ArgumentNullException(nameof(package));
+			commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+			var menuCommandID = new CommandID(CommandSet, CommandId);
+			var menuItem = new MenuCommand(Execute, menuCommandID);
+			commandService.AddCommand(menuItem);
+		}
+		public static CopySelectionCommand Instance
+		{
+			get;
+			private set;
+		}
+
+		private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider => package;
+
+		public static async Task InitializeAsync(AsyncPackage package)
+		{
+			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+			var commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+			Instance = new CopySelectionCommand(package, commandService);
+		}
+
+		private void Execute(object sender, EventArgs e)
+		{
+			_ = ExecuteAsync(sender, e);
+		}
+
+		private async Task ExecuteAsync(object sender, EventArgs e)
+		{
+			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+			var dte = await ServiceProvider.GetServiceAsync(typeof(DTE)) as DTE;
+			var doc = dte?.ActiveDocument;
+			if (doc != null)
+			{
+				var textDoc = doc.Object() as TextDocument;
+				var selection = doc.Selection as TextSelection;
+				var language = textDoc.Language;
+				var rangeEnumerator = selection.TextRanges.GetEnumerator();
+				var idx1 = selection.TopLine;
+				var idx2 = selection.BottomLine;
+				if (idx1 < idx2)
+				{
+					var text = textDoc.CreateEditPoint(textDoc.StartPoint).GetLines(idx1, idx2 + 1);
+					var lines = Regex.Split(text, "\\r+\\n+").ToList();
+					while (lines.Count > 0 && lines.First().Trim().Length == 0)
+						lines.RemoveAt(0);
+					while (lines.Count > 0 && lines.Last().Trim().Length == 0)
+						lines.RemoveAt(lines.Count - 1);
+					if (lines.Count > 0)
+					{
+						while (lines[0].Length > 0)
+						{
+							var c = lines[0].Substring(0, 1);
+							if (lines.Any(line => line.StartsWith(c) == false))
+								break;
+							lines = lines.Select(line => line.Substring(1)).ToList();
+						}
+						var result = lines.Aggregate("", (prev, curr) => $"{prev}{(string.IsNullOrEmpty(prev) ? "" : "\n")}{curr}");
+						if (language != "Plain Text")
+							result = $"```{language}\n{result}\n```";
+						System.Windows.Forms.Clipboard.SetText(result);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ CopyWithMinIndentPackage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace CopyWithMinIndent
+{
+	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+	[Guid(PackageGuidString)]
+	[ProvideMenuResource("Menus.ctmenu", 1)]
+	public sealed class CopyWithMinIndentPackage : AsyncPackage
+	{
+		public const string PackageGuidString = "69ec4646-9854-404e-96f5-878231b9c5f8";
+
+		protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+		{
+			await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+			await CopySelectionCommand.InitializeAsync(this);
+		}
+	}
+}
